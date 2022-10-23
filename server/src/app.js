8a44db152ad81a91c20b51658cd17025b6fd44b7
LeeYuSeung <akows141@gmail.com>
2022-10-23 21:36:39 +0900

    [프로젝트 배포 프로젝트 수정] - 스크립트 코드를 수정함.

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static('build'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is Running`))
